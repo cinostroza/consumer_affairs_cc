a9ac86079baeb31118fb634c4dfaa1bff282348c
cinostroza <c.inostroza@gmail.com>
2021-11-20 21:37:02 -0300

    Creating Event Model

--- the_eye/models.py
+++ the_eye/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    session_id = models.CharField(blank=False, max_length=250)
+    category = models.CharField(blank=False, max_length=100)
+    name = models.CharField(blank=False, max_length=100)
+    data = models.TextField()
+    timestamp = models.DateTimeField(blank=False)
