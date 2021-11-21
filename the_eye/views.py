68f383aea0424eae3e8d440af760b00fc7fe3797
cinostroza <c.inostroza@gmail.com>
2021-11-20 22:31:01 -0300

    Added urls and improved the model

--- the_eye/views.py
+++ the_eye/views.py
@@ -3,6 +3,9 @@ from the_eye.serializers import EventSerializer
 from rest_framework import generics
 
 
+# TODO: Add authentications
+# TODO: Add specific views to query based on sessions, event types, timestamps etc..
+
 class EventList(generics.ListCreateAPIView):
     queryset = Events.objects.all()
     serializer_class = EventSerializer
