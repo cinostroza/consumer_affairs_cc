d6a033a09885fa5225817bb3e6f7c7bdc26414aa
cinostroza <c.inostroza@gmail.com>
2021-11-22 22:33:42 -0300

    Started the work on the validator

--- the_eye/datavalidator.py
+++ the_eye/datavalidator.py
@@ -1,3 +1,10 @@
 from the_eye.models import RawEventsData, Events
 
+
+# This task should be queued in Celery
 # TODO: Create the validator
+def data_validator():
+    raw_data = RawEventsData.objects.all()
+    for raw_event in raw_data:
+        # validate the data and once validated save it to the Events table and delete the RawEvent
+        pass
